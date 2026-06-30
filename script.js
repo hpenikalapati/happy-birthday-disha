05f2c6d9f19c2e8576293b2d456ade7853e0265e
Hansita Penikalapati <138614461+hpenikalapati@users.noreply.github.com>
2026-06-30 12:05:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 function showMessage(){
-    document.getElementById("surprise").classList.toggle("show");
+
+    document
+        .getElementById("surprise")
+        .classList.toggle("show");
 
     confetti({
-        particleCount:150,
-        spread:80,
-        origin:{y:0.6}
+
+        particleCount:180,
+
+        spread:90,
+
+        origin:{y:.6}
+
     });
+
+}
+
+
+
+// CREATE SPARKLES
+
+const sparkleContainer = document.getElementById("sparkles");
+
+for(let i=0;i<35;i++){
+
+    let sparkle=document.createElement("div");
+
+    sparkle.className="sparkle";
+
+    sparkle.style.left=Math.random()*100+"vw";
+
+    sparkle.style.top=Math.random()*100+"vh";
+
+    sparkle.style.animationDelay=Math.random()*3+"s";
+
+    sparkle.style.animationDuration=2+Math.random()*3+"s";
+
+    sparkleContainer.appendChild(sparkle);
+
 }
